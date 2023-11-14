83422b1f7082dd1ac5c804aafc8af7968d1d86ca
Artiom Porcescu <porchiesku@bk.ru>
2023-11-14 11:52:01 +0200

    Initial Commit

---
+++ BitTrust/BitTrustApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitTrustApp.swift
+//  BitTrust
+//
+//  Created by Artiom Porcescu on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BitTrustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

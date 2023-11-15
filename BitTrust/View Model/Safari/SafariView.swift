5d3f810c244ad382f40b76048ed8c4f0ea1c912c
Artiom Porcescu <porchiesku@bk.ru>
2023-11-15 14:06:01 +0200

    Set up news list for viewing them in Safari view

---
+++ BitTrust/View Model/Safari/SafariView.swift
@@ -0,0 +1,18 @@
+//
+//  SafariView.swift
+//  BitTrust
+//
+//  Created by Artiom Porcescu on 15.11.2023.
+//
+
+import SwiftUI
+
+struct SafariView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SafariView()
+}

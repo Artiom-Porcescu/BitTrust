0052420b309832d749c7475b51c44167c0c11287
Artiom Porcescu <porchiesku@bk.ru>
2023-11-15 10:53:22 +0200

    Finished setting up view model for getting news URLs and titles

---
+++ BitTrust/View Model/GetNewsItems/RequestNewsItemViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RequestNewsItemViewModel.swift
+//  BitTrust
+//
+//  Created by Artiom Porcescu on 14.11.2023.
+//
+
+import Foundation
---
+++ BitTrust/View/NewsFeed/NewsView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsView.swift
+//  BitTrust
+//
+//  Created by Artiom Porcescu on 14.11.2023.
+//
+
+import SwiftUI
+
+struct NewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsView()
+}

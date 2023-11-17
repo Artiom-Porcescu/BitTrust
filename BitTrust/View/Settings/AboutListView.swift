f4df6baea5af6fe02dd922510dbdfa4ff586e943
Artiom Porcescu <porchiesku@bk.ru>
2023-11-17 18:10:46 +0200

    Created About section in Settings tab, later on will refactor the code to reuse each line, improve code

---
+++ BitTrust/View/Settings/AboutListView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutListView.swift
+//  BitTrust
+//
+//  Created by Artiom Porcescu on 17.11.2023.
+//
+
+import SwiftUI
+
+struct AboutListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutListView()
+}
